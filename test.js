05bed92d9cdb2c3113a1671ea98f5091f14f3998
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-07-18 10:13:35 +0530

    Test n stuff

--- test.js
+++ test.js
@@ -1,3 +1,21 @@
+import dotenv from 'dotenv';
+const thingy = require('dotenv');
+thingy.config();
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{"role": "system", "content": "You are a helpful assistant."}, {role: "user", content: "Hello world"}],
+});
+
+console.log(completion.data.choices[0].message);
+
 console.log("Howdy folks");
 
 // a for loop going thru a list of names
@@ -8,17 +26,17 @@ for (var i = 0; i < names.length; i++) {
 
 let thingy = document.getElementById("thingy");
 // change the text of the thingy
-thingy.innerHTML = "THis be chaging";
+thingy.innerHTML = "This be chaging";
 
 let buttonThingy = document.getElementById("buttonThingy");
 // add an event listener to the button
-buttonThingy.addEventListener("click", test2);
+buttonThingy.addEventListener("click", GPTTest);
 
 //define a function test that changes thingy to something else
 function test() {
     thingy.innerHTML = "This is a test";
 }
 
-function test2() {
-    thingy.innerHTML = "This is a test2";
+function GPTTest() {
+    thingy.innerHTML = completion.data.choices[0].message;
 }
