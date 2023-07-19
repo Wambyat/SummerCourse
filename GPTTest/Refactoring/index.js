93e5e3a4c9729972ef5aa5c181894d11669d3b6b
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-07-19 09:08:46 +0530

    initializing refactoring

---
+++ GPTTest/Refactoring/index.js
@@ -0,0 +1,3 @@
+// const setupTextArea = document.getElementById('setup-textArea')
+// const setupInputContainer = document.getElementById('setup-input-container')
+// const movieBossText = document.getElementById('movie-boss-text')
